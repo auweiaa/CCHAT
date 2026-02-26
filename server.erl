21718287a9df17faa8d36e46b7bdb6f5068a0a08
auweiaa <tim.hunker@hhu.de>
2026-02-26 21:41:10 +0100

    WIP: first attempt of start function.

--- server.erl
+++ server.erl
@@ -8,7 +8,16 @@ start(ServerAtom) ->
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    not_implemented.
+    
+    case whereis(ServerAtom) of
+        undefined ->
+                    ServerPid = spawn(fun() -> msgLoop() end),
+                    true = register(ServerAtom, ServerPid),
+                    ServerPid;
+
+        _Pid -> {error, already_exists}
+    end.
+
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
@@ -16,3 +25,9 @@ stop(ServerAtom) ->
     % TODO Implement function
     % Return ok
     not_implemented.
+
+%ToDo: implement handling of incoming messages:
+msgLoop() ->
+    receive
+        _ -> io:format("New Message~n")
+    end, msgLoop().
