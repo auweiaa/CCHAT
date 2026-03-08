96287fa5504a0025f6adafffc38a0289ffcea077
auweiaa <tim.hunker@hhu.de>
2026-03-08 21:00:42 +0100

    add stop function

--- server.erl
+++ server.erl
@@ -5,22 +5,15 @@
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    
     Pid = genserver:start(ServerAtom, initState, fun handler/2),
     Pid.
-
 
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+    genserver:stop(ServerAtom),
+    ok.
 
 
 % ---------------------------------------------------------
@@ -31,6 +24,10 @@ joinChannel(ServerAtom) ->
 
 leaveChannel(ServerAtom) ->
     genserver:request(ServerAtom, join_channel).
+
+
+% ToDo:
+% changed Nickname in channels
 
 
 % ---------------------------------------------------------
